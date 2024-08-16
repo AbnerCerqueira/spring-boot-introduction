ad54cec65bdaaa122a12d7b8df8ebc11c29b02d7
abnercerqueira <abner.cerqueira@fatec.sp.gov.br>
2024-08-15 23:18:02 -0300

    :tada: Commit Inicial

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories
+public class DemoApplication implements CommandLineRunner {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+    
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/errors/WrongPasswordException.java
@@ -0,0 +1,5 @@
+//package com.example.demo.errors;
+//
+//public class WrongPasswordException extends RuntimeException {
+//    super
+//}
